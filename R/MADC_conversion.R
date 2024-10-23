08f7b94910d6808e36659c82db99a4496084f469
Cristianetaniguti <chaytaniguti@gmail.com>
2024-10-23 16:30:01 -0400

    convert csv to vcf

--- R/MADC_conversion.R
+++ R/MADC_conversion.R
@@ -1,48 +1,83 @@
-library(vcfR)
-library(pwalign)
-library(Biostrings)
-library(parallel)
 
-# report_csv <- read.csv("DSp23-8876_MADC_rename_LSU_filter_miss.csv")
-# result_csv <- read.csv("DSp23-8876_MADC_rename_LSU_filter_miss_snps.csv")
-# result_vcf <- read.vcfR("DSp23-8876_MADC_rename_LSU_filter_miss_snps.vcf")
-#
-# # Comparing when botloci
-# head(report_csv[,1:4],7)
-# head(result_csv[,1:7],13)
-# head(result_vcf@fix,7)
-# head(result_vcf@gt[,1:2],7)
-#
-# # Comparing when not botloci
-# report_csv[grep("Chr01_000239479", report_csv$AlleleID),1:4]
-# result_csv[grep("Chr01_000239479", result_csv$AlleleID),1:7]
-# result_vcf@fix[grep("Chr01_000239479", result_vcf@fix[,3]),]
-# result_vcf@gt[grep("Chr01_000239479", result_vcf@fix[,3]),1:2]
-#
-# # Inputs
-# botloci <- read.csv("sweetpotato_20K_SNPset_f180bp_forDArT_3K_f180bp.botloci", header = F)
-# botloci <- botloci$V1
-# had_seq <- read.table("sweetpotato_allele_db_v001.fa", header = F)
-# report <- read.csv("DSp23-8876_MADC_rename_LSU_filter_miss.csv")
-# n.cores <- 5
-#
-# my_results_csv <- loop_though_dartag_report(report, botloci, hap_seq, n.cores=5, verbose = TRUE)
-#
-# # Compare with python result
-# dim(result_csv)
-# result_csv <- result_csv[order(result_csv$Chromosome, result_csv$SNP_position_in_Genome),]
-# my_results_csv$SNP_position_in_Genome <- as.numeric(my_results_csv$SNP_position_in_Genome)
-# my_results_csv <- my_results_csv[order(my_results_csv$Chromosome, my_results_csv$SNP_position_in_Genome),]
-#
-# sum(!result_csv$AlleleID %in% my_results_csv$AlleleID)
-# dim(my_results_csv)[1] - dim(result_csv)[1]
+#' Converts MADC file to VCF recovering target and off-target SNPs
+#' @param madc path to MADC file
+#' @param botloci path to file containing the target IDs that were designed in the bottom strand
+#' @param hap_seq path to haplotype DB fasta file
+#' @param rm_multiallelic_SNP logical. If TRUE, SNP with more than one alternative base will be removed. If FALSE, check `multiallelic_SNP_dp_thr` specs
+#' @param multiallelic_SNP_dp_thr numerical. If `rm_multiallelic_SNP` is FALSE, set a total depth by tag threshold to eliminate low frequency SNP allele. If the threshold does not eliminate the multiallelic aspect of the marker, the marker is discarded. This is likely to happen to paralogous sites.
+#' @param n.cores number of cores to be used in the parallelization
+#' @param out_vcf output VCF file name
+#' @param verbose print metrics on the console
+#'
+#' @import vcfR
+#' @export
+get_OffTargets <- function(madc = NULL,
+                           botloci = NULL,
+                           hap_seq = NULL,
+                           n.cores = 5,
+                           rm_multiallelic_SNP = FALSE,
+                           multiallelic_SNP_dp_thr = 0,
+                           out_vcf = NULL,
+                           verbose = TRUE){
 
+  bigr_meta <- paste0('##BIGrCommandLine.getOffTargets=<ID=getOffTargets,Version="',
+                      packageVersion("BIGr"), '",Data="',
+                      Sys.time(),'", CommandLine="> getOffTargets(',deparse(substitute(madc)),', ',
+                      "botloci= ", botloci, ', ',
+                      "hap_seq= ", hap_seq, ', ',
+                      "n.cores= ", n.cores, ', ',
+                      "rm_multiallelic_SNP= ", rm_multiallelic_SNP, ', ',
+                      "multiallelic_SNP_dp_thr= ", multiallelic_SNP_dp_thr, ', ',
+                      "out_vcf= ", out_vcf, ', ',
+                      "verbose= ", verbose,')">')
+
+  report <- read.csv(madc)
+  botloci <- read.csv(botloci, header = F)
+  hap_seq <- read.table(hap_seq, header = F)
+
+  my_results_csv <- loop_though_dartag_report(report, botloci, hap_seq,
+                                              n.cores=n.cores, verbose = verbose)
+
+  vcf_body <- create_VCF_body(my_results_csv, n.cores = n.cores,
+                              rm_multiallelic_SNP = rm_multiallelic_SNP,
+                              multiallelic_SNP_dp_thr = multiallelic_SNP_dp_thr,
+                              verbose = verbose)
+
+
+  #Make a header separate from the dataframe
+  vcf_header <- c(
+    "##fileformat=VCFv4.3",
+    "##reference=NA",
+    "##contig=<ID=NA,length=NA>",
+    '##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">',
+    '##INFO=<ID=ADS,Number=R,Type=Integer,Description="Depths for the ref and each alt allele in the order listed">',
+    '##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read depth">',
+    '##FORMAT=<ID=RA,Number=1,Type=Integer,Description="Reference allele read depth">',
+    '##FORMAT=<ID=AD,Number=R,Type=Integer,Description="Allelic depths for the ref and alt alleles in the order listed">',
+    bigr_meta
+  )
+
+  gz <- gzfile(paste0(out_vcf, ".gz"), "w")
+  write(vcf_header, gz)
+  header <- colnames(vcf_body)
+  header <- paste(header, collapse="\t")
+  write(header, gz)
+  close(gz)
+  vcfR:::.write_vcf_body(fix = as.matrix(vcf_body[,1:8]), gt = as.matrix(vcf_body[,9:ncol(vcf_body)]), filename = paste0(out_vcf, ".gz"))
+}
 
 #' Include SNP_position_in_Genome, Ref, and Alt information
 #'
+#' @param report MADC file
+#' @param botloci file containing the target IDs that were designed in the bottom strand
+#' @param hap_seq haplotype DB fasta file
+#' @param n.cores number of cores to be used in the parallelization
+#' @param verbose print metrics on the console
+#'
+#' @import parallel
 loop_though_dartag_report <- function(report, botloci, hap_seq, n.cores=1, verbose = TRUE){
 
-  had_seq <- get_ref_alt_hap_seq(had_seq)
+  hap_seq <- get_ref_alt_hap_seq(hap_seq)
 
   nsamples <- ncol(report) - 3
   new.file <- data.frame("AlleleID" = report[,1],
@@ -53,8 +88,8 @@ loop_though_dartag_report <- function(report, botloci, hap_seq, n.cores=1, verbo
   by_cloneID <- split.data.frame(new.file, new.file$CloneID)
 
   clust <- makeCluster(n.cores)
-  clusterExport(clust, c("had_seq","add_ref_alt"))
-  add_ref_alt_results <- parLapply(clust, by_cloneID, function(x) add_ref_alt(x, had_seq))
+  clusterExport(clust, c("hap_seq","add_ref_alt"))
+  add_ref_alt_results <- parLapply(clust, by_cloneID, function(x) add_ref_alt(x, hap_seq))
   stopCluster(clust)
 
   updated_by_cloneID <- lapply(add_ref_alt_results, "[[",1)
@@ -89,12 +124,12 @@ loop_though_dartag_report <- function(report, botloci, hap_seq, n.cores=1, verbo
   return(my_results_csv)
 }
 
-#' Check if Ref_0001 and Alt_0002 tags are present, if not, add them from the had_seq input
+#' Check if Ref_0001 and Alt_0002 tags are present, if not, add them from the hap_seq input. Function made for parallelization.
 #'
-#' @param one_tag
-#' @param had_seq
+#' @param one_tag madc file split by tag
+#' @param hap_seq haplotype DB
 #'
-add_ref_alt <- function(one_tag, had_seq) {
+add_ref_alt <- function(one_tag, hap_seq) {
 
   # Add ref and alt
   cloneID <- one_tag$CloneID[1]
@@ -104,24 +139,24 @@ add_ref_alt <- function(one_tag, had_seq) {
   # Precompute ref and alt indices
   ref_index <- alt_index <- 0
 
-  # Use match() for faster lookup in had_seq
-  ref_row <- match(ref, had_seq$AlleleID)
-  alt_row <- match(alt, had_seq$AlleleID)
+  # Use match() for faster lookup in hap_seq
+  ref_row <- match(ref, hap_seq$AlleleID)
+  alt_row <- match(alt, hap_seq$AlleleID)
 
   # Initialize a list to store potential new rows to add
   new_rows <- list()
 
   if (!ref %in% one_tag$AlleleID) {
     ref_index <- 1
-    # Only extract the relevant sequence data once from had_seq
-    ref_seq <- if (!is.na(ref_row)) had_seq[ref_row, 2] else NA
+    # Only extract the relevant sequence data once from hap_seq
+    ref_seq <- if (!is.na(ref_row)) hap_seq[ref_row, 2] else NA
     empty_allele <- c(ref, rep(NA, 4), cloneID, ref_seq, rep(0, nsamples))
     new_rows[[length(new_rows) + 1]] <- empty_allele
   }
 
   if (!alt %in% one_tag$AlleleID) {
     alt_index <- 1
-    alt_seq <- if (!is.na(alt_row)) had_seq[alt_row, 2] else NA
+    alt_seq <- if (!is.na(alt_row)) hap_seq[alt_row, 2] else NA
     empty_allele <- c(alt, rep(NA, 4), cloneID, alt_seq, rep(0, nsamples))
     new_rows[[length(new_rows) + 1]] <- empty_allele
   }
@@ -140,8 +175,8 @@ add_ref_alt <- function(one_tag, had_seq) {
 #' Discard tags if alternative in the target locus is N
 #' Do the complement reverse if cloneID present in the botloci vector
 #'
-#' @param one_tag
-#' @param botloci
+#' @param one_tag madc file split by tag
+#' @param botloci file containing the target IDs that were designed in the bottom strand
 #'
 #' @importFrom Biostrings DNAString reverseComplement
 #' @importFrom pwalign pairwiseAlignment nucleotideSubstitutionMatrix
@@ -241,15 +276,16 @@ compare <- function(one_tag, botloci){
 #' Converts the fasta to a data.frame with first column the AlleleID and and second the AlleleSequence
 #' The function will work even if the sequence is split in multiple lines
 #'
-#' @param hap_seq
+#' @param hap_seq haplotype db
+#'
 get_ref_alt_hap_seq <- function(hap_seq){
-  headers <- had_seq$V1[grep(">",had_seq$V1)]
+  headers <- hap_seq$V1[grep(">",hap_seq$V1)]
   headers <- gsub(">", "", headers)
 
-  seqs <- had_seq$V1[-grep(">",had_seq$V1)]
+  seqs <- hap_seq$V1[-grep(">",hap_seq$V1)]
 
-  times <- diff(grep(">",had_seq$V1))-1
-  last <- length(had_seq$V1) - grep(">",had_seq$V1)[length(grep(">",had_seq$V1))]
+  times <- diff(grep(">",hap_seq$V1))-1
+  last <- length(hap_seq$V1) - grep(">",hap_seq$V1)[length(grep(">",hap_seq$V1))]
   times <- c(times, last)
   index <- vector()
   for(i in 1:length(headers)){
@@ -264,4 +300,144 @@ get_ref_alt_hap_seq <- function(hap_seq){
 }
 
 
+#' Creates VCF body from CSV generated by loop_though_dartag_report
+#'
+#' @param csv CSV file generated by loop_though_dartag_report
+#' @param rm_multiallelic_SNP logical. If TRUE, SNP with more than one alternative base will be removed. If FALSE, check `multiallelic_SNP_dp_thr` specs
+#' @param multiallelic_SNP_dp_thr numerical. If `rm_multiallelic_SNP` is FALSE, set a total depth by tag threshold to eliminate low frequency SNP allele. If the threshold does not eliminate the multiallelic aspect of the marker, the marker is discarded. This is likely to happen to paralogous sites.
+#' @param n.cores number of cores to be used in the parallelization
+#' @param verbose print metrics on the console
+#'
+#' @import parallel
+create_VCF_body <- function(csv, rm_multiallelic_SNP = TRUE, multiallelic_SNP_dp_thr = 10, n.cores = 1, verbose = TRUE){
 
+  #csv <- my_results_csv
+  cloneID <- split.data.frame(csv, csv$CloneID)
+
+  clust <- makeCluster(n.cores)
+  clusterExport(clust, c("merge_counts","rm_multiallelic_SNP", "multiallelic_SNP_dp_thr"))
+  #clusterExport(clust, c("merge_counts"))
+  vcf_tag_list <- parLapply(clust, cloneID, function(x) merge_counts(x, rm_multiallelic_SNP, multiallelic_SNP_dp_thr))
+  stopCluster(clust)
+
+  vcf_tag_list1 <- lapply(vcf_tag_list, "[[", 1)
+  rm_mks <- sapply(vcf_tag_list, "[[" ,2)
+
+  if(verbose) print(paste("SNP removed because presented more than one allele:", sum(rm_mks)))
+
+  vcf_body <- do.call("rbind",vcf_tag_list1)
+  vcf_body <- as.data.frame(vcf_body)
+  vcf_body$V2 <- as.numeric(vcf_body$V2)
+  vcf_body <- vcf_body[order(vcf_body[,1], vcf_body[,2]),]
+  rownames(vcf_body) <- NULL
+  colnames(vcf_body) <- c("#CHROM", "POS", "ID", "REF", "ALT","QUAL", "FILTER", "INFO","FORMAT", colnames(csv)[-c(1:7)])
+
+  return(vcf_body)
+}
+
+
+#' Function made for parallelization of create_VCF_body function
+#'
+#' @param cloneID_unit one item of csv file split by cloneID
+#' @param rm_multiallelic_SNP logical. If TRUE, SNP with more than one alternative base will be removed. If FALSE, check `multiallelic_SNP_dp_thr` specs
+#' @param multiallelic_SNP_dp_thr numerical. If `rm_multiallelic_SNP` is FALSE, set a total depth by tag threshold to eliminate low frequency SNP allele. If the threshold does not eliminate the multiallelic aspect of the marker, the marker is discarded. This is likely to happen to paralogous sites.
+#'
+merge_counts <- function(cloneID_unit, rm_multiallelic_SNP = FALSE, multiallelic_SNP_dp_thr = 0){
+
+  #Get counts for target SNP
+  rm <- 0
+  RefTag <- apply(cloneID_unit[which(grepl("Ref", cloneID_unit$AlleleID) & !duplicated(cloneID_unit$AlleleID)),-c(1:7)], 2, sum)
+  AltTag <- apply(cloneID_unit[which(grepl("Alt", cloneID_unit$AlleleID) & !duplicated(cloneID_unit$AlleleID)),-c(1:7)], 2, sum)
+  tab_counts <- paste0(RefTag + AltTag, ":", RefTag, ":", RefTag, ",", AltTag)
+
+  info <- cloneID_unit[grep("Ref_", cloneID_unit$AlleleID),]
+  info <- c(info$Chromosome,
+            info$SNP_position_in_Genome,
+            paste0(info$Chromosome, "_", info$SNP_position_in_Genome),
+            info$Ref, info$Alt, ".", ".", paste0("DP=", sum(c(RefTag, AltTag)),";",
+                                                 "ADS=",sum(RefTag),",",sum(AltTag)), "DP:RA:AD")
+
+  vcf_tag <- c(info, tab_counts)
+
+  # Check if there are more than one alternative allele by loci
+  off_tag <- cloneID_unit[-which(grepl("Ref_", cloneID_unit$AlleleID) | grepl("Alt_", cloneID_unit$AlleleID)),]
+  if(nrow(off_tag)){ # If there are off target SNP
+    by_pos <- split.data.frame(off_tag, off_tag$SNP_position_in_Genome)
+
+    ## Possibility to filter alt alleles with low read
+    # The filtering is done after the counts are collapsed so all reads can be counted
+    for(i in 1:length(by_pos)){
+      alleles <- unique(by_pos[[i]]$AlleleID)
+
+      if(length(unique(by_pos[[i]]$Alt)) > 1){ # If SNP is multiallelic
+
+        if(rm_multiallelic_SNP){ # option to remove multiallelics
+          rm <- rm + 1
+          break()
+        } else if(multiallelic_SNP_dp_thr > 0){ # If not removed, user can set threshold to remove low frequency alleles
+          rm.idx <- which(apply(by_pos[[i]][,-c(1:7)], 1, sum) < multiallelic_SNP_dp_thr)
+          if(length(rm.idx))  up_by_pos <- by_pos[[i]][-rm.idx,] else up_by_pos <- by_pos[[i]]
+
+          if(length(unique(up_by_pos$Alt)) == 0) { # If after applied filter all tags are gone
+            rm <- rm + 1
+            break()
+
+          } else if (length(unique(up_by_pos$Alt)) > 1 ){ # If after applied filter the SNP remains multiallelic
+            by_alt <- split.data.frame(up_by_pos, up_by_pos$Alt)
+            by_alt_counts <- lapply(by_alt, function(x) apply(x[,-c(1:7)], 2, sum))
+            total_counts <- sapply(by_alt_counts, sum)
+            by_alt_counts <- by_alt_counts[order(total_counts, decreasing = T)] # Most frequency base will come first
+            total_alt <- 0
+            for(j in 1:length(by_alt_counts)){
+              total_alt <- total_alt + by_alt_counts[[j]]
+              if(j == 1) alt <- by_alt_counts[[j]] else  alt <- paste0(alt, ",",by_alt_counts[[j]])
+            }
+            info <- up_by_pos[,2:5]
+            info$Alt <- paste0(names(by_alt_counts), collapse = ",")
+            info <- unique.data.frame(info)
+
+          } else { # If after applied filter, only one alternative remains
+            alt <- apply(up_by_pos[,-c(1:7)], 2, sum)
+            total_alt <- alt
+            info <- unique.data.frame(up_by_pos[,2:5])
+          }
+
+        } else { # If rm_multiallelic_SNP set to FALSE and threshold is 0, keep all multiallelics
+          by_alt <- split.data.frame(by_pos[[i]], by_pos[[i]]$Alt)
+          by_alt_counts <- lapply(by_alt, function(x) apply(x[,-c(1:7)], 2, sum))
+          total_counts <- sapply(by_alt_counts, sum)
+          by_alt_counts <- by_alt_counts[order(total_counts, decreasing = T)] # Most frequency base will come first
+          total_alt <- 0
+          for(j in 1:length(by_alt_counts)){
+            total_alt <- total_alt + by_alt_counts[[j]]
+            if(j == 1) alt <- by_alt_counts[[j]] else  alt <- paste0(alt, ",",by_alt_counts[[j]])
+          }
+
+          info <- by_pos[[i]][,2:5]
+          info$Alt <- paste0(names(by_alt_counts), collapse = ",")
+          info <- unique.data.frame(info)
+        }
+      } else { # If SNP is not multiallelic
+        up_by_pos <- by_pos[[i]]
+        alt <- apply(up_by_pos[,-c(1:7)], 2, sum)
+        total_alt <- alt
+        info <- unique.data.frame(by_pos[[i]][,2:5])
+      }
+
+      ref <- apply(cloneID_unit[-which(cloneID_unit$AlleleID %in% alleles | duplicated(cloneID_unit$AlleleID)),-c(1:7)], 2, sum)
+      tab_counts <- paste0(ref + total_alt, ":", ref, ":", ref, ",", alt)
+
+      info <- c(info$Chromosome,
+                info$SNP_position_in_Genome,
+                paste0(info$Chromosome, "_", info$SNP_position_in_Genome),
+                info$Ref, info$Alt, ".", ".", paste0("DP=", sum(c(ref, total_alt)),";",
+                                                     "ADS=",sum(ref),",",sum(total_alt)), "DP:RA:AD")
+
+      vcf_off_tag <- c(info, tab_counts)
+
+      vcf_tag <- rbind(vcf_tag, vcf_off_tag)
+    }
+  }
+
+  return(list(vcf_tag, rm))
+}

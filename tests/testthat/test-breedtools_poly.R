80be4b2128e4811a3fa31aea34021818c15f2c9d
alex-sandercock <sandercock.alex@gmail.com>
2025-03-27 09:58:51 -0400

    Added Tests

---
+++ tests/testthat/test-breedtools_poly.R
@@ -0,0 +1,31 @@
+context("BreedTools")
+
+
+test_that("test breedtools poly",{
+  #Input variables
+  ref_file <- system.file("test_ref.txt", package="BIGr")
+  val_file <- system.file("test_test.txt", package="BIGr")
+  ref_ids <- system.file("ref_ids.txt", package="BIGr")
+
+  #import files
+  reference = read.table(ref_file, header = T, row.names = 1, sep = "\t")
+  validation = read.table(val_file, header = T, row.names = 1, sep = "\t")
+  reference_ids = read.table(ref_ids, header = T, sep = "\t")
+
+  #Calculations
+  ref_ids = lapply(as.list(reference_ids),as.character)
+
+  freq = allele_freq_poly(reference, ref_ids, ploid = 4)
+
+  prediction = as.data.frame(solve_composition_poly(validation,freq, ploidy = 4))
+
+  #Check
+  freq_mean <- round(mean(as.numeric(freq)),6)
+  pred_mean <- round(mean(as.numeric(prediction$R2)),6)
+
+
+  expect_equal(freq_mean, 0.888889, tolerance = 0.01)
+  expect_equal(pred_mean, 0.841454, tolerance = 0.01)
+  expect_true(nrow(prediction) == 175)
+
+})
---
+++ tests/testthat/test-check_ped.R
@@ -0,0 +1,20 @@
+context("Imputation Concordance")
+
+
+test_that("test imputation",{
+  #Input variables
+  ped_file <- system.file("check_ped_test.txt", package="BIGr")
+
+  #Calculations
+  output.list <- check_ped(ped_file, TRUE)
+
+  #Check
+  df_length <- length(output.list)
+  messy_parents <- output.list$messy_parents
+  missing_parents <- output.list$missing_parents
+
+  expect_true(df_length == 2)
+  expect_true(all(messy_parents$id == c("grandfather2","grandfather3")))
+  expect_true(nrow(missing_parents) == 13)
+
+})

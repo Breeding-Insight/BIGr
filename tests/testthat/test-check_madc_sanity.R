86b4fef4e24b827595036749cad51b2a9e645f27
Alexander Sandercock <39815775+alex-sandercock@users.noreply.github.com>
2026-04-20 11:12:20 -0400

    added marker_id support

--- tests/testthat/test-check_madc_sanity.R
+++ tests/testthat/test-check_madc_sanity.R
@@ -68,4 +68,101 @@ test_that("check madc",{
   expect_equal(res$checks, exp)
 })
 
+test_that("check_botloci remaps using Marker_ID", {
+  botloci <- data.frame(V1 = c("1_0001", "2_0002"))
+  report <- data.frame(
+    CloneID = c("ProbeA_0001", "ProbeB_0002"),
+    AlleleID = c("ProbeA_0001|Ref_0001", "ProbeB_0002|Ref_0001"),
+    AlleleSequence = c("A", "T"),
+    check.names = FALSE
+  )
+  mi_df <- data.frame(
+    Marker_ID = c("ProbeA_0001", "ProbeB_0002"),
+    Chr = c("1", "2"),
+    Pos = c(1, 2)
+  )
 
+  res <- check_botloci(botloci, report, ChromPos = FALSE, mi_df = mi_df, verbose = FALSE)
+
+  expect_equal(res[[2]]$CloneID, botloci$V1)
+  expect_equal(res[[3]]$CloneID, botloci$V1)
+})
+
+test_that("check_botloci resolves Marker_ID before padding report CloneIDs", {
+  botloci <- data.frame(V1 = "1_000000123")
+  report <- data.frame(
+    CloneID = "1_123",
+    AlleleID = "1_123|Ref_0001",
+    AlleleSequence = "A",
+    check.names = FALSE
+  )
+  mi_df <- data.frame(
+    Marker_ID = "1_123",
+    Chr = "1",
+    Pos = 123
+  )
+
+  res <- check_botloci(botloci, report, ChromPos = TRUE, mi_df = mi_df, verbose = FALSE)
+
+  expect_equal(res[[2]]$CloneID, botloci$V1)
+  expect_equal(res[[3]]$CloneID, botloci$V1)
+})
+
+test_that("pick_markers_info_id_col scores distinct markers not allele rows", {
+  mi_df <- data.frame(
+    CloneID = c("m1", "m2"),
+    Marker_ID = c("m1", "m3")
+  )
+  query_ids <- c("m1", "m1", "m1", "m2")
+
+  expect_equal(pick_markers_info_id_col(mi_df, query_ids), "CloneID")
+})
+
+test_that("check_madc_sanity returns FALSE for malformed CloneID positions", {
+  report <- data.frame(
+    CloneID = c("Chr_abc", "Chr_abc"),
+    AlleleID = c("Chr_abc|Ref_0001", "Chr_abc|Alt_0002"),
+    AlleleSequence = c("A", "T"),
+    check.names = FALSE
+  )
+
+  res <- check_madc_sanity(report)
+
+  expect_false(is.na(res$checks["ChromPos"]))
+  expect_false(res$checks["ChromPos"])
+})
+
+test_that("check_botloci errors if widening MADC padding still does not match", {
+  botloci <- data.frame(V1 = "1_0002")
+  report <- data.frame(
+    CloneID = "1_1",
+    AlleleID = "1_1|Ref_0001",
+    AlleleSequence = "A",
+    check.names = FALSE
+  )
+
+  expect_error(
+    check_botloci(botloci, report, ChromPos = TRUE, verbose = FALSE),
+    "After matching padding, botloci markers still not found in MADC file. Check marker IDs."
+  )
+})
+
+test_that("check_botloci keeps AlleleID synchronized after CloneID remap", {
+  botloci <- data.frame(V1 = "1_0001")
+  report <- data.frame(
+    CloneID = "ProbeA_0001",
+    AlleleID = "ProbeA_0001|Ref_0001",
+    AlleleSequence = "A",
+    check.names = FALSE
+  )
+  mi_df <- data.frame(
+    Marker_ID = "ProbeA_0001",
+    Chr = "1",
+    Pos = 1
+  )
+
+  res <- check_botloci(botloci, report, ChromPos = TRUE, mi_df = mi_df, verbose = FALSE)
+
+  expect_equal(res[[2]]$CloneID, "1_0001")
+  expect_equal(res[[2]]$AlleleID, "1_0001|Ref_0001")
+})
